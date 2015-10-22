6e4a2ed4645dbdc89ebffad7834f1f9509508c10
Dylan <stolen3@gmail.com>
2015-10-22 12:44:43 -0700

    dist

--- dist/deadbolt.js
+++ dist/deadbolt.js
@@ -1,46 +1,42 @@
+ /** @namespace */
 var deadbolt = (function() {
     var queue = [];
     var validators = {
         email: function() {
-            return new RegExp("(.+)@(.+)\.(.+)", "i");
+            return new RegExp("^(.+?)\@(.+?)(\.[a-zA-Z.]{2,})*$", "i");
         },
-        minLength: function(what) {
-            return new RegExp("^(.+){" + what + ",}$", "i");
+        phone: function() {
+        	return new RegExp("((\\d)?\\(?(\\d){3}\\)?)?(\\s|-)?(\\d){3}(\\s|-)?(\\d){4}");
         },
-        not: function(what) {
-            return new RegExp("^((?!" + what + ").)*$", "i");
+        minLength: function(val) {
+            return new RegExp("^(.+){" + val + ",}$", "i");
+        },
+        not: function(val) {
+            return new RegExp("^((?!" + val + ").)*$", "i");
         },
         digit: function() {
-            return new RegExp("^[0-9,\-]+$", "i");
+            return new RegExp("^[0-9]+$", "i");
         }
     };
 
+    /** @constructor */
     function deadbolt(elem, options) {
         return new deadbolt.fn._init(elem, options);
     }
 
-	/* Internal Functions */
-	/* _init: Initializes deadbolt - loads in form elements with corresponding varifications. / Returns: OBJECT:(deadbolt) */
-	/* _tester: Tests an element with its varifications to see if it passes. / Returns: BOOLEAN:(True / False) */
     deadbolt.fn = deadbolt.prototype = {
         _init: function(elem, options) {
         	var qElem = document.querySelector(elem).querySelectorAll("[data-valid]");
 
-        	// Check if any rules are passed into instantiation function. If so, add it to the global validators object.
-            if(options !== undefined)
-            {
+            if(options !== undefined) {
                 for(var key in options)
                     validators[key] = options[key];
             }
-
-            // Search form for data-valid attributes, then create an array combining those rules with the DOM element
             for(var i = 0; i < qElem.length; i++) {
                 var validation = [],
                 	tempElem = qElem[i], 
                 	what = (tempElem.dataset.valid.indexOf(",") > 0)? tempElem.dataset.valid.split(",") : [tempElem.dataset.valid];
 
-                // Check all rules on a DOM element. 
-                // If they match rules defined in the global validators, add the corresponding RegExp function to local validation array
                 for (var t = 0; t < what.length; t++) {
                     for (var key in validators) {
                         if (what[t].match(new RegExp(key, "i")))
@@ -53,31 +49,61 @@ var deadbolt = (function() {
             return deadbolt;
         },
         _tester: function(input) {
-        	var what = input[0], validator = input[1], hasError = input[2];
+        	var what = input[0], validator = input[1];
 
             for (var i = 0; i < validator.length; i++) {
                 if (!validator[i].test(what.value)) {
-                    hasError = true;
-
+                    input[2] = true;
                     return false;
                 }
-                hasError = false;
             }
+            input[2] = false;
 
             return true;
         }
     };
 
-	/* External Functions */
-	/* isValid: Checks if all the queued elements match their rules, if so return true or false. / Returns: BOOLEAN(True / False) */
-    deadbolt.isValid = deadbolt.prototype = function() {
+	/**
+	 * Checks if elements in the queue are valid based on the rules provided.
+	 * @param {function} callback - Call a function once isValid() is complete.
+	 * @memberOf deadbolt
+	 * @function
+
+	 * @returns {boolean}
+	 */
+    deadbolt.isValid = deadbolt.prototype = function(callback) {
         var tempBool = true;
         for (var i = 0; i < queue.length; i++) {
             if (deadbolt.fn._tester(queue[i]) === false)
                 tempBool = false;
         }
+        callback(deadbolt);
 
         return tempBool;
+    }
+
+	/**
+	 * Applies class names to input element's parent node if the input is valid or not.
+	 * @memberOf deadbolt
+	 * @function
+
+	 * @returns {deadbolt}
+	 */
+    deadbolt.applyClass = deadbolt.prototype = function() {
+    	for (var i = 0; i < queue.length; i++) {
+    		var elem = queue[i][0].parentNode;
+
+    		if(queue[i][2] === true) {
+    			elem.classList.remove("has-success");
+    			elem.classList.add("has-error");
+    		}
+    		else {
+    			elem.classList.remove("has-error");
+    			elem.classList.add("has-success");
+    		}
+    	}
+
+    	return deadbolt;
     }
 
     return deadbolt;
